da5be271030008b4037c15ba1fd3b0a069f51f86
Anderson Ocaña <andru.ocatorres@gmail.com>
2023-06-11 13:08:00 +0000

    add butn

--- sinapsis_web/apps_sinapsis/login_app/urls.py
+++ sinapsis_web/apps_sinapsis/login_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from apps_sinapsis.login_app.views import LandingPage,LoginPage,RegisterPage
+from django.contrib.auth.views import LoginView
+
+from apps_sinapsis.login_app.views import LandingPage,RegisterPage
 
 urlpatterns = [
     path('', LandingPage.as_view( ), name='landing_page'),
-    path('login/',LoginPage.as_view(),name='login'),
+    path('login/',LoginView.as_view(),name='login'),
     path('register/',RegisterPage.as_view(),name='register'),
 ]
 
--- sinapsis_web/apps_sinapsis/login_app/views.py
+++ sinapsis_web/apps_sinapsis/login_app/views.py
@@ -1,4 +1,8 @@
 from django.views.generic.base import TemplateView
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 
 class LandingPage(TemplateView):
@@ -12,3 +16,19 @@ class LoginPage(TemplateView):
 
 class RegisterPage(TemplateView):
     template_name='register.html'
+    
+    
+    
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return response
